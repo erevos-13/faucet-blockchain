398c271da9577ad7e53040a60ecbc6b5c9785b87
Orfeas voutsaridis <ov@wappier.com>
2022-01-04 20:11:00 +0200

    update migration

--- contracts/FaucetContract.sol
+++ contracts/FaucetContract.sol
@@ -1,12 +1,24 @@
 pragma solidity >=0.4.22 <0.9.0;
 
+
+
 contract Faucet {
     // storage variables
 
-    uint public funds = 1000; // positive values only
-    int public counter = -10;
+    // uint public funds = 1000; // positive values only
+    // int public counter = -10;
 
-    uint32 public test = 4294967295;
+    // uint32 public test = 4294967295;
     
+    // // funds [0x323423] = 500 
+
+    // this is spacial function
+    // it's called when you mkae a trancasion that doesn't specify function name to call
+
+    // Exteranl function are part of the contract interaface 
+    // which means they can be called via contract and other trancstion 
+
+    receive() external payable {}
 
 }
+
